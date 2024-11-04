bd8e1fa9c1973f6962388f5933c312d178e42bbe
KingsukNandi <kingsuk24nandi@gmail.com>
2024-11-05 01:10:14 +0530

    Setup fonts

--- src/components/globals/Navbar.tsx
+++ src/components/globals/Navbar.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Navbar = () => {
-  return (
-    <div className=''>Navbar</div>
-  )
-}
+  return <div className="">Navbar</div>;
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["montserrat"],
+        imfell: ["imfell"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
