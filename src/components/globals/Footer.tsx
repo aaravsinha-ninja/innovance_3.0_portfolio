e87a88496b18eb2a5731691d93c2d59c731ee167
KingsukNandi <kingsuk24nandi@gmail.com>
2024-11-05 19:49:53 +0530

    Add navbar and footer

--- src/components/globals/Footer.tsx
+++ src/components/globals/Footer.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import SocialMediaLinks from "./SocialMediaLinks";
+import { footer } from "../../data/data";
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="text-sm font-montserrat flex justify-between items-center py-6 px-28">
+      <div>&copy;&nbsp;{footer.copyRightText}</div>
+      <div>
+        <a href={`mailto:${footer.email}`}>{footer.email}</a>
+      </div>
+      <SocialMediaLinks />
+    </div>
+  );
+};
---
+++ src/components/globals/SocialMediaLinks.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { socials } from "../../data/data";
+
+const SocialMediaLinks = () => {
+  return (
+    <div className="flex gap-5">
+      {socials &&
+        socials.map((social) => (
+          <div key={social.name}>
+            <a href={social.link} target="_blank" rel="noopener noreferrer">
+              <img
+                src={social.icon}
+                alt={social.name}
+                className="aspect-square w-8"
+              />
+            </a>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default SocialMediaLinks;
