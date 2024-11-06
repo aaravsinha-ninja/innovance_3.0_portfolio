c606ca5e87b36cb8575b2531f2aa1fa92385ad1b
Archi Shaw <archishaw2004@gmail.com>
2024-11-06 22:55:01 +0530

    update

--- src/components/main/MyAdvantage/MyAdvantage.tsx
+++ src/components/main/MyAdvantage/MyAdvantage.tsx
@@ -83,7 +83,7 @@ export const MyAdvantage = () => {
     <div className="flex flex-col items-center">
       <hr className="w-10/12 border-1 border-solid border-gray-400 my-6" />
       <h3 className="text-center text-2xl font-medium my-6 font-imfell">My Advantage</h3>
-      <div className="flex flex-wrap justify-center gap-6 my-8 mx-4">
+      <div className="flex flex-wrap justify-center gap-14 my-8 mx-4">
         {myadv.map((skill, index) => (
           <div
             key={index}
