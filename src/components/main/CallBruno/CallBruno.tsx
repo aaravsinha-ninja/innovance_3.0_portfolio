57b9f40bd58ff79ca3ccc0c6b2ee68f778294609
KingsukNandi <kingsuk24nandi@gmail.com>
2024-11-06 22:40:39 +0530

    change arrow size

--- src/components/main/CallBruno/CallBruno.tsx
+++ src/components/main/CallBruno/CallBruno.tsx
@@ -19,7 +19,7 @@ export const CallBruno = () => {
           <img
             src="https://cdn-icons-png.flaticon.com/512/2223/2223606.png"
             alt="arrow"
-            className="w-9 md:w-14 pt-6 md:pt-6"
+            className="w-8 md:w-11 pt-4 md:pt-6"
           />
         </div>
       </div>
