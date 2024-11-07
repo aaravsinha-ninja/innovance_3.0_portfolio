9a8ab7c878fbf7a311fbf27b8d2eb9cc3ce055e9
KingsukNandi <kingsuk24nandi@gmail.com>
2024-11-07 21:41:30 +0530

    update

--- src/components/main/MyAdvantage/MyAdvantage.tsx
+++ src/components/main/MyAdvantage/MyAdvantage.tsx
@@ -70,14 +70,14 @@ export const MyAdvantage = () => {
             return (
               <div
                 key={index}
-                className="flex flex-col items-center hover:text-blue-500"
+                className={`flex flex-col items-center hover:text-blue-500 ${
+                  gray ? "grayscale hover:grayscale-0" : ""
+                }`}
               >
                 <div className="rounded-full w-32 h-40 flex flex-col items-center justify-center bg-gray-100 shadow-md transition-all duration-300 transform hover:scale-105 hover:bg-blue-100">
                   <img
                     src={skill.image}
-                    className={`w-20 h-20 mb-2 ${
-                      gray ? "grayscale hover:grayscale-0" : ""
-                    }`}
+                    className={`w-20 h-20 mb-2`}
                     alt={`${skill.name} icon`}
                   />
                   <div className="text-xl font-semibold">
@@ -93,7 +93,7 @@ export const MyAdvantage = () => {
             return (
               <div
                 key={index}
-                className="flex flex-col items-center hover:text-blue-500"
+                className="flex flex-col items-center hover:text-blue-500 grayscale hover:grayscale-0"
                 onMouseOver={() => {
                   setGray(true);
                 }}
@@ -101,7 +101,7 @@ export const MyAdvantage = () => {
                 <div className="rounded-full w-32 h-40 flex flex-col items-center justify-center bg-gray-100 shadow-md transition-all duration-300 transform hover:scale-105 hover:bg-blue-100">
                   <img
                     src={skill.image}
-                    className="w-20 h-20 mb-2 grayscale hover:grayscale-0"
+                    className="w-20 h-20 mb-2"
                     alt={`${skill.name} icon`}
                   />
                   <div className="text-xl font-semibold">
