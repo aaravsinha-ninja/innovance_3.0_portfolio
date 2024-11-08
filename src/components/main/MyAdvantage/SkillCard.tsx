cbcd0c36e50fe35d31abec12d911116f040a01b1
Undercrashers <2306127@kiit.ac.in>
2024-11-08 19:04:42 +0530

    figma updated

--- src/components/main/MyAdvantage/SkillCard.tsx
+++ src/components/main/MyAdvantage/SkillCard.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {MyAdv} from "../../../types/types";
 
-const SkillCard: React.FC<{skill: MyAdv; isFirst: boolean}> = ({skill, isFirst}) => (
-    <div className={`flex flex-col items-center hover:text-blue-500 ${isFirst ? "" : "grayscale hover:grayscale-0"}`}>
+const SkillCard: React.FC<{skill: MyAdv}> = ({skill}) => (
+    <div className={`flex flex-col items-center hover:text-blue-500 grayscale hover:grayscale-0`}>
         <div className="rounded-full w-32 h-40 flex flex-col items-center justify-center bg-gray-100 shadow-md transition-all duration-300 transform hover:scale-105 hover:bg-blue-100">
             <img src={skill.image} className="w-20 h-20 mb-2" alt={`${skill.name} icon`} />
             <div className="text-xl font-semibold">{skill.proficiency}</div>
